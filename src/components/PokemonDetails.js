7fb8007b4c02ec1d867d54a49b658b9c66459e96
AymenBida <aymen.bida.00@gmail.com>
2021-07-21 19:20:18 +0100

    Style individual pokemon page

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -27,11 +27,32 @@ const PokemonDetails = () => {
   }
 
   return (
-    <div>
-      <h2>{capitalize(currentPokemon.name)}</h2>
-      <img src={currentPokemon.image} alt="" />
-      <h3>{`Type 1: ${capitalize(currentPokemon.type1)}`}</h3>
-      <h3>{`Type 2: ${(currentPokemon.type2 !== null) ? capitalize(currentPokemon.type2) : 'none'}`}</h3>
+    <div className="row w-75 m-auto mt-5 gx-3 d-flex justify-content-between">
+      <div className="col-4 border border-dark rounded p-3 d-flex flex-column align-items-center justify-content-around">
+        <h2 className="text-center">{`${currentPokemon.id}. ${capitalize(currentPokemon.name)}`}</h2>
+        <img className="w-75 text-center" src={currentPokemon.image} alt="" />
+      </div>
+      <div className="col-8 row border border-dark rounded px-3 py-5 d-flex">
+        <div className="col-4">
+          <h3>Type 1:</h3>
+          <p className="h4 mb-4">{capitalize(currentPokemon.type1)}</p>
+          <h3>Type 2:</h3>
+          <p className="h4">{(currentPokemon.type2 !== null) ? capitalize(currentPokemon.type2) : 'none'}</p>
+        </div>
+        <div className="d-flex flex-column col-8">
+          <h3 className="mb-3 ">Stats:</h3>
+          <div className="d-flex flex-wrap">
+            {Object.entries(currentPokemon.stats).map((stat) => (
+              <p className="w-50 h5 mb-3" key={stat[0]}>
+                {`${capitalize(stat[0])}: `}
+                <span className="h4">{stat[1]}</span>
+              </p>
+            ))}
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 };
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -8,10 +8,19 @@ export const getPokemon = (response) => ({
 export const seeDetails = ({ data }) => ({
   type: SEE_DETAILS,
   payload: {
+    id: data.id,
     name: data.name,
     image: data.sprites.front_default,
     type1: data.types[0].type.name,
     type2: data.types[1]?.type.name ?? null,
+    stats: {
+      hp: data.stats[0].base_stat,
+      attack: data.stats[1].base_stat,
+      defense: data.stats[2].base_stat,
+      spAttack: data.stats[3].base_stat,
+      spDefense: data.stats[4].base_stat,
+      speed: data.stats[5].base_stat,
+    },
   },
 });
 
