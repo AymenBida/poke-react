7dfe198ae7241c244c4200151e5a2ae928a37524
AymenBida <aymen.bida.00@gmail.com>
2021-07-24 21:39:14 +0100

    fix linters

--- src/__test__/components/Filter.test.js
+++ src/__test__/components/Filter.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import Filter from '../../components/presentationals/Filter';
+import Filter from '../../components/Filter';
 import filterReducer from '../../redux/reducers/filter';
 
 const state = '';
